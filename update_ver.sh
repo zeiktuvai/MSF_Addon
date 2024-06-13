233efa94e24a5e8c15e5fe43cf78db4b36d3ca90
zeiktuvai <zeik.tuvaii@gmail.com>
2024-06-12 22:23:18 -0400

    added version script and file

---
+++ update_ver.sh
@@ -0,0 +1,15 @@
+VER="1.5"
+
+find . -name 'version_V*' -delete
+echo "MSF Release Version: "$VER >| version_V"$VER"
+echo "Updated MSF version"
+cat version_V"$VER"
+
+find Publish/ -name 'version_V*' -delete
+echo "MSF Release Version: "$VER >| Publish/version_V"$VER"
+echo "Updated Publish Version:"
+cat Publish/version_V"$VER"
+
+sed -i "s\version =.*\version = $VER\g" MSF/config.cpp
+echo "MSF Config updated"
+grep "version" MSF/config.cpp | sed -e 's/^\s*//' -e '/^$/d' 
