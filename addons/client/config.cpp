6e39083f6bcb6a2eb8650da3e36a3b31f5b00124
zeiktuvai <zeik.tuvaii@gmail.com>
2024-06-20 22:45:06 -0400

    Trying to see why variables are not being saved. #MSF-1

--- addons/client/config.cpp
+++ addons/client/config.cpp
@@ -20,14 +20,14 @@ class CfgUnitInsignia
     {
 		displayName = "Task Force Yodel Insignia";
 		author = "zeik_tuvai";
-		texture = "z\msf\assons\client\patch\TFY.paa";
+		texture = "z\msf\addons\client\patch\TFY.paa";
 		textureVehicle = "";
     };
     class tfy_backup_pilot
     {
         displayName = "TFY Backup Pilot";
         author = "Samurai";
-        texture = "z\msf\assons\client\patch\TFY_backup_pilot.paa";
+        texture = "z\msf\addons\client\patch\TFY_backup_pilot.paa";
         textureVehicle = "";
     };
 };
--- addons/main/cfg/object/MSF_Attr_General.hpp
+++ addons/main/cfg/object/MSF_Attr_General.hpp
@@ -21,7 +21,7 @@ class MSFGeneralFunctions
 			tooltip = "This percentage sets the damage reduction to all incoming damage for this vehicle.  (i.e. at 75%, incoming fire will only do 25% damage.) Caps at 95%";
 			property = "MSF_General_DmgReduction";
 			control = "Slider";
-			expression = "if (_value > 0.95) then { _this setVariable ['%s',0.95]; } else { _this setVariable ['%s',_value]; };";
+			expression = "if (_value > 0.95) then { _this setVariable ['%s',0.95,true]; } else { _this setVariable ['%s',_value,true]; };";
 			defaultValue = 0;			
 			condition = "objectVehicle";
 		};
