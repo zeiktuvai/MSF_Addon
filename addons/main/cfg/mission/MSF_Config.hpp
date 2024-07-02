c87c37deffa3f7ce8dc2176689ca598ae4967f6f
zeiktuvai <zeik.tuvaii@gmail.com>
2024-07-01 22:36:33 -0400

    Removed test loadout from config. MSF-3

--- addons/main/cfg/mission/MSF_Config.hpp
+++ addons/main/cfg/mission/MSF_Config.hpp
@@ -19,34 +19,5 @@ class MSFConfig
 			Turrets[] = {"RHS_AGS30_TriPod_MSV","rhs_KORD_MSV","rhs_KORD_high_MSV","RHS_NSV_TriPod_MSV","RHS_ZU23_MSV"};
 			Units[] = {"rhs_msv_emr_armoredcrew", "rhs_msv_emr_rifleman", "rhs_msv_emr_sergeant", "rhs_msv_emr_machinegunner", "rhs_msv_emr_LAT"};
 		};
-	};
-	class BaseLoadouts
-	{
-		class TFY_Marksman
-		{			
-			weapons[] = {
-				"rhs_weap_m14ebrri","Rangefinder","rhsusf_weap_glock17g4"
-			};
-			magazines[] = {
-				"acex_intelitems_notepad", "HandGrenade", "HandGrenade", "SmokeShellGreen", "SmokeShellRed", "rhsusf_20Rnd_762x51_m993_Mag", 
-				"rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", 
-				"rhsusf_mag_17Rnd_9x19_FMJ", "rhsusf_mag_17Rnd_9x19_FMJ", "rhsusf_mag_17Rnd_9x19_FMJ", "rhsusf_mag_17Rnd_9x19_FMJ", "rhsusf_20Rnd_762x51_m993_Mag", 
-				"rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", "rhsusf_20Rnd_762x51_m993_Mag", 
-				"rhsusf_mag_17Rnd_9x19_JHP"
-			};
-			items[] = {
-				"ACE_EarPlugs", "ACE_MapTools", "ACE_DefusalKit", "ACE_M26_Clacker", "ACE_IR_Strobe_Item", "ACE_CableTie", "ACE_HuntIR_monitor", "ACE_WaterBottle", 
-				"ACE_bloodIV", "ACE_packingBandage", "ACE_packingBandage", "ACE_packingBandage", "ACE_packingBandage", "ACE_packingBandage", "ACE_quikclot", 
-				"ACE_quikclot", "ACE_quikclot", "ACE_quikclot", "ACE_quikclot", "ACE_elasticBandage", "ACE_elasticBandage", "ACE_elasticBandage", "ACE_elasticBandage", 
-				"ACE_elasticBandage", "ACE_tourniquet", "ACE_tourniquet", "ACE_splint", "ACE_splint", "ACE_epinephrine", "ACE_epinephrine", "ACE_morphine", 
-				"ACE_morphine", "ACE_morphine", "ACE_morphine", "ACE_adenosine", "ACE_adenosine", "ACE_salineIV_500", "ACE_salineIV_500", "ACE_Canteen", "ACE_microDAGR"
-			};
-			linkedItems[] = {
-				"V_PlateCarrier2_rgr", "H_HelmetB_camo", "G_Balaclava_TI_tna_F", "ItemMap", "ItemCompass", "ACE_Altimeter", "ItemGPS", "JAS_GPNVG18_Tan_TI", 
-				"rhsusf_acc_anpeq15side_bk", "optic_DMS", "rhsusf_acc_harris_bipod"
-			};
-			uniformClass = "U_B_T_Soldier_SL_F";
-			backpack = "B_AssaultPack_tna_F";
-		};
-	};
+	};	
 };
