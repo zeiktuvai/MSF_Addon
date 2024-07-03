005c9102b7d5a82ab0a7dd9272af6d1d1965dd40
zeiktuvai <zeik.tuvaii@gmail.com>
2024-07-03 15:43:44 -0400

    added new weapons MSF-3 MSF-12

---
+++ addons/units/weapons/MSF_rhs_hk416d10.hpp
@@ -0,0 +1,16 @@
+class rhs_weap_hk416d10;
+class MSF_rhs_hk416d10 : rhs_weap_hk416d10
+{
+	scope = 1;
+	displayName = "MSF HK416d10";
+	class LinkedItems {
+		class LinkedItemsOptic {
+			slot = "CowsSlot";
+			item = "rhsusf_acc_su230";
+		};
+		class LinkedItemsUnder {
+			slot = "UnderBarrelSlot";
+			item = "rhsusf_acc_grip2";
+		};
+	};
+};
