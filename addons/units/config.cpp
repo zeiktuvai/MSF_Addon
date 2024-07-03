1474d68eec9f83caaeb1ebae571a216ca7b119de
zeiktuvai <zeik.tuvaii@gmail.com>
2024-07-03 15:43:09 -0400

    Updated config MSF-3 MSF-12

--- addons/units/config.cpp
+++ addons/units/config.cpp
@@ -1,5 +1,8 @@
 #include "..\main\MSF_Version.hpp"
 #include "MSF_Functions.hpp"
+#include "MSF_Roles.hpp"
+#include "MSF_Faction.hpp"
+#include "MSF_Categories.hpp"
 
 class CfgPatches
 {
@@ -11,71 +14,8 @@ class CfgPatches
         units[] = {};
         weapons[] = {};
         requiredVersion = 2.0;
-        requiredAddons[] = { "3DEN", "cba_settings", "ace_interaction", "NATO_Winter_Units" };
-    };
-};
-
-class CfgFactionClasses {
-    class BLU_F_MSF {
-        displayName = "NATO (MSF)";
-        flag = "\a3\Data_f\Flags\flag_nato_co.paa";
-        icon = "\a3\Data_f\cfgFactionClasses_BLU_ca.paa";
-        priority = 3;
-        side = 1;
-    };
-};
-
-class CfgEditorSubcategories
-{
-    class MSF_EdSubcat_Inf_Trop {
-        displayname = "Infantry (Tropic)";
-    };
-    class MSF_EdSubcat_Inf_Night {
-        displayname = "Infantry (Night)";
-    };
-};
-
-class CfgRoles
-{
-    class MSFCommand
-    {
-        displayName = "Leader";
-        icon = "a3\ui_f\data\igui\rscingameui\rscunitinfo\role_commander_ca.paa";
-    };
-    class MSFAssault
-    {
-        displayName = "Assault";
-        icon = "a3\ui_f\data\gui\cfg\respawnroles\assault_ca.paa";
-    };
-    class MSFRecon
-    {
-        displayName = "Recon";
-        icon = "a3\ui_f\data\gui\cfg\respawnroles\recon_ca.paa";
-    };
-    class MSFSupport
-    {
-        displayName = "Support";
-        icon = "a3\ui_f\data\gui\cfg\respawnroles\support_ca.paa";
-    };
-    class MSFHeavy
-    {
-        displayName = "Heavy";
-        icon = "a3\ui_f\data\igui\rscingameui\rscunitinfo\role_gunner_ca.paa";
-    };
-    class MSFCrew
-    {
-        displayName = "Vehicle Crew";
-        icon = "a3\ui_f\data\igui\rscingameui\rscunitinfo\role_driver_ca.paa";
-    };
-    class MSFPilot
-    {
-        displayName = "Flight Crew";
-        icon = "a3\modules_f_curator\data\portraitcas_ca.paa";
-    };
-    class MSFSpecial
-    {
-        displayName = "Special Forces";
-        icon = "a3\ui_f\data\gui\cfg\respawnroles\recon_ca.paa";
+        requiredAddons[] = { "3DEN", "cba_settings", "ace_interaction", "NATO_Winter_Units", "rhsusf_c_troops", 
+        "rhsusf_sounds", "rhsusf_c_weapons" };
     };
 };
 
@@ -84,8 +24,11 @@ class CfgWeapons {
     #include "weapons\MSF_rhs_hk416_m320.hpp"
     #include "weapons\MSF_rhs_m249.hpp"
     #include "weapons\MSF_rhs_hk416d.hpp"
+    #include "weapons\MSF_rhs_hk416d10.hpp"
+    #include "weapons\MSF_rhs_M107.hpp"
 };
 class CfgVehicles {
     #include "vehicles\MSF_Soldier_Base.hpp"
+    #include "vehicles\MSF_Sniper_Base.hpp"
     #include "vehicles\MSF_Units.hpp"
 };
---
+++ addons/units/vehicles/MSF_Sniper_Base.hpp
@@ -0,0 +1,16 @@
+class B_T_Sniper_F;
+class MSF_Sniper_Base : B_T_Sniper_F
+{
+	displayName = "Rifleman";
+    icon = "iconMan";
+	role = "MSFAssault";
+	author = "Zeik_Tuvai";
+	faction = "BLU_F_MSF";
+	class eventhandlers
+	{
+		class MSF_Init
+		{
+			init = "[_this select 0] call MSF_fnc_ApplyInsignia;";
+		};
+	};
+};
