e14e69935ca2a81c525dd9fb90a640d1d2cbae47
zeiktuvai <zeik.tuvaii@gmail.com>
2024-09-27 15:40:12 -0400

    updated msf soldier base to call EH function instead of insignia function.

--- MSF-Units/addons/units/vehicles/blufor/MSF_Soldier_Base.hpp
+++ MSF-Units/addons/units/vehicles/blufor/MSF_Soldier_Base.hpp
@@ -30,7 +30,7 @@ class MSF_Soldier_Base : B_Soldier_base_f
 	{
 		class MSF_Insignia
 		{
-			init = "[_this select 0] call MSF_fnc_ApplyInsignia;";
+			init = "[_this select 0] call MSF_fnc_SetInsigniaEH;";
 		};
 		class MSF_Radio
 		{
