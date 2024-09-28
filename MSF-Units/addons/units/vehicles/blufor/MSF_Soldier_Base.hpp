c284fe6184f27606653987a5815ebc20542c0254
zeiktuvai <zeik.tuvaii@gmail.com>
2024-09-28 19:57:47 -0400

    Fixed bug in soldier base init.

--- MSF-Units/addons/units/vehicles/blufor/MSF_Soldier_Base.hpp
+++ MSF-Units/addons/units/vehicles/blufor/MSF_Soldier_Base.hpp
@@ -30,7 +30,7 @@ class MSF_Soldier_Base : B_Soldier_base_f
 	{
 		class MSF_Insignia
 		{
-			init = "[_this select 0] call MSF_fnc_SetInsigniaEH;";
+			init = "[_this select 0] call MSF_fnc_ApplyInsignia;";
 		};
 		class MSF_Radio
 		{
