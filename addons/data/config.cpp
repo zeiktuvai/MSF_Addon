c1e162bf4d5f3f0abfeafa3347d6399019ade4eb
zeiktuvai <zeik.tuvaii@gmail.com>
2025-03-26 13:19:11 -0400

    Moved client to data

---
+++ addons/data/config.cpp
@@ -0,0 +1,15 @@
+#include "..\main\cfg\MSF_Macros.hpp"
+
+class CfgPatches
+{
+    class ADDON
+    {
+      name = "MSF - Data";
+      author = "Zeik_Tuvai";
+      version = MSF_VERSION;
+      units[] = {};
+      weapons[] = {};
+      requiredVersion = 1.0;
+      requiredAddons[] = { "MSF3den" };
+    };
+};
--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 2
 #define MINOR 12
 #define PATCH 2
-#define BUILD 1309
+#define BUILD 1310
